eb57e3b7031442bb63320c04e5fdaaa2a3b15270
Isaac <isaac211.md@outlook.com>
2025-01-29 19:26:10 -0300

    swagger + server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,56 @@
 import express, {Express, Response} from "express";
-//import dotenv from "dotenv";
 import cors from 'cors';
 import helmet from 'helmet';
-
+import swaggerUi from 'swagger-ui-express';
 import routes from '../routes'
 import * as process from "node:process";
-
-//dotenv.config();
+import swaggerJSDoc from 'swagger-jsdoc';
 
 const server = express();
-//const port: string | number = process.env.PORT || 8000;
+//Configuration Swagger
+/*const options: swaggerJSDoc.Options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Documentation',
+            version: '1.0.0',
+            description: 'Documentación de la API',
+        },
+        servers: [
+            {
+                url: 'http://localhost:8000',
+            },
+        ],
+    },
+    apis: ['./src/routes/*.ts'],
+};
+const swaggerSpec = swaggerJSDoc(options);*/
+
+//const fs = require("fs");
+const path = require("path");
+
+server.use("/public", express.static(path.resolve(__dirname, "../../public")));
+
+// Generar la UI de Swagger
+const swaggerHTML = swaggerUi.generateHTML(undefined, {
+    swaggerOptions: {
+        url: "/swagger.json",
+        explorer: true,
+    },
+});
+
+// Servir Swagger UI
+server.use("/docs", swaggerUi.serve);
+server.get("/docs", (req, res) => {
+    res.send(swaggerHTML);
+});
 
 server.use(
     '/api',
     routes);
+
+//Static server
+server.use(express.static('public'));
 
 server.use(helmet());
 server.use(cors());
